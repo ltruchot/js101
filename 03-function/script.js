d227b0860bf1de3f34bf46cdd4e6f8104b3b4528
SamHn <shanini@adneom.com>
2017-02-07 14:44:10 +0100

    feat(function): functions exercices

---
+++ 03-function/script.js
@@ -0,0 +1,64 @@
+var pets = ['cat','dog','fish','monkey']
+
+var so_team = ['Anais','Thomas','Marine','Bo','Guillaume','Aurélien','Eliot']
+
+var special_sentence = "You're the best!"
+
+
+/**
+* A function has the keyword function
+* A function has a name & () after the name
+* A function can take a parameter or multiple parameters
+* A function ca return something
+* A function can call another function
+*/
+
+console.log('Script is loaded !');
+
+
+
+
+// STEP 1: Create your first function
+/* Instructions: 
+ * - create a function to show all the pets'name inside your console
+ */
+
+
+
+
+
+// STEP 2: Call a function
+/* Instructions: 
+ * - call your function to see on the console all the pets'name
+ */
+
+
+
+
+ // STEP 2: Create a new function with parameter
+/* Instructions: 
+ * - Create a function you can use with pets or so_team
+ * - Call the method
+ */
+
+
+
+
+
+ // STEP 3: Create 2 functions
+/* Instructions: 
+ * - Create a function which print every member of the SO TEAM
+ * - Create a function which print the special sentences 
+ * - Print on the screen every name of the SO_team
+ * - After every name you have to print the special sentence
+ */
+
+
+
+
+
+ // STEP 4: Create a return function
+/* Instructions: 
+ * - Create a function who return a result of a calculation a + b
+ * - Print the data of the result
+ */
