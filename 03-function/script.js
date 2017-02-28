9079cbb0c8a4b3e09c5f5d2fee8e11258b59b410
Loïc TRUCHOT <ltruchot@adneom.com>
2017-02-28 17:44:07 +0100

    surname for animals

--- 03-function/script.js
+++ 03-function/script.js
@@ -1,64 +1,64 @@
-var pets = ['cat','dog','fish','monkey']
+var pets = ['Raymond the cat','Tina the dog','Milili the fish','Tom the monkey', 'Edgard the dolphin', 'Twenty the pinguin', 'Sara the unicorn'];
 
-var so_team = ['Anais','Thomas','Marine','Bo','Guillaume','Aurélien','Eliot']
+var secrets =  ['I love you.', 'I want you to touch me...', 'Pet me pleeaaaaase master !', 'You are beautiful, but you upset me.', 'you have beautiful hair...', 'I need you mommy !', "Can i lick your hand ?"];
 
-var special_sentence = "You're the best!"
+var so_team = ['Anais','Thomas','Marine','Bo','Guillaume','Aurélien','Eliot'];
+
+var special_sentence = " is the best!";
 
 
 /**
 * A function has the keyword function
 * A function has a name & () after the name
 * A function can take a parameter or multiple parameters
-* A function ca return something
+* A function can return something
 * A function can call another function
 */
-
 console.log('Script is loaded !');
 
 
 
 
 // STEP 1: Create your first function
-/* Instructions: 
+/* Instructions:
  * - create a function to show all the pets'name inside your console
  */
 
 
-
-
-
 // STEP 2: Call a function
-/* Instructions: 
+/* Instructions:
  * - call your function to see on the console all the pets'name
  */
 
 
-
-
  // STEP 2: Create a new function with parameter
-/* Instructions: 
+/* Instructions:
  * - Create a function you can use with pets or so_team
  * - Call the method
  */
 
 
 
-
-
  // STEP 3: Create 2 functions
-/* Instructions: 
- * - Create a function which print every member of the SO TEAM
- * - Create a function which print the special sentences 
- * - Print on the screen every name of the SO_team
- * - After every name you have to print the special sentence
+/* Instructions:
+ * - Create a function which print one member of the SO TEAM & the special sentence, given as arguments
  */
 
-
-
-
-
- // STEP 4: Create a return function
-/* Instructions: 
+  // STEP 4: Create a return function
+/* Instructions:
  * - Create a function who return a result of a calculation a + b
  * - Print the data of the result
  */
+
+
+// STEP 5: random function
+// following return a random element of the array 'arr'
+var arr = ['test1', 'test2', 'test3']
+var randomElement = arr[Math.floor(Math.random() * arr.length)];
+console.log(randomElement);
+// create a function that return a random element of any table
+
+
+// STEP 6: use functions in function
+// create a function that log a sentence like this:
+// '<pet> say to <so team member>: "<secret>"
